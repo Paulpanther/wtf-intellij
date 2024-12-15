7b58f3fe3a12faff56c840b6ea4766fac8e5f4eb
paulmethfessel <paul.methfessel@web.de>
2024-12-15 16:41:15 +0100

    init project

---
+++ src/main/kotlin/com/paulmethfessel/wtfintellij/MyIcons.kt
@@ -0,0 +1,7 @@
+package com.paulmethfessel.wtfintellij
+
+import com.intellij.openapi.util.IconLoader
+
+object MyIcons {
+  val lang = IconLoader.getIcon("/icons/lang.svg", javaClass)
+}
---
+++ src/main/kotlin/com/paulmethfessel/wtfintellij/WtfLang.kt
@@ -0,0 +1,6 @@
+package com.paulmethfessel.wtfintellij
+
+import com.intellij.lang.Language
+
+object WtfLang: Language("WTF") {
+}
